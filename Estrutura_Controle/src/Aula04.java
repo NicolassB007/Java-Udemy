7663631ab82d04633abd34cad1535d5b80c69def
Nicolas Bissotto <nicolasbissottomeusestudos@gmail.com>
2025-02-13 00:09:54 -0300

    Estrutura de repetição While

---
+++ Estrutura_Controle/src/Aula04.java
@@ -0,0 +1,11 @@
+public class Aula04 {
+    public static void main(String[] args){
+        var counter = 0;
+
+        while (counter <= 9) {
+            System.out.println("Bom dia");
+            counter++;
+        }
+
+    }
+}
