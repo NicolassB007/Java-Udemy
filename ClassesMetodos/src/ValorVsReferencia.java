b3b8368985a1ab6731a688792c0d8d2093c579fe
Nicolas Bissotto <nicolasbissottomeusestudos@gmail.com>
2025-02-24 19:52:50 -0300

    ....

--- ClassesMetodos/src/ValorVsReferencia.java
+++ ClassesMetodos/src/ValorVsReferencia.java
@@ -1,6 +1,19 @@
+import java.util.Date;
+
 public class ValorVsReferencia {
     public static void main(String[] args) {
         int a = 2;
-        int b = a; // Atribuição por valor;
+        int b = a; // Atribuição por valor (Tipo Primitivo);
+
+        a++;
+        b--;
+
+        System.out.println(a + " " + b);
+
+        Date d1 = new Date();
+        Date d2 = d1; // Atribuição por referência (Objeto);
+
+        /*
+        * Um método STATIC só pode acessar outro método static, regra*/
     }
 }
---
+++ ExerciciosOrientacaoObjetos/src/PrimeiroTrauma.java
@@ -0,0 +1,8 @@
+public class PrimeiroTrauma {
+
+    int a = 3;
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
